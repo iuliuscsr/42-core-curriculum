37104aa9bb2a2b71c56a19eb2f75b00e9902130a
Julius Malsam <jmalsam@2-E-1.42heilbronn.de>
2026-06-19 17:20:13 +0200

    upload ex5 proto

---
+++ Milestone_2/python_01/ex5/ft_plant_types.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+class Plant:
+    """this is a class of a plant"""
+    def __init__(self, name: str, height: float, age: int) -> None:
+        """initializes a new plant"""
+        self.name = name
+        self.set_height(height, flag=True)
+        self.set_age(age, flag=True)
+
+    def set_height(self, height: float, flag: bool = False) -> None:
+        """protects data from wrong parameters"""
+
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            print("Height update rejected")
+            if flag:
+                self._height = 15.0
+        else:
+            self._height = height
+            if not flag:
+                print(f"Height updated: {round(self._height)}cm")
+
+    def set_age(self, age: int, flag: bool = False) -> None:
+        """protects data from wrong parameters"""
+        if age < 0:
+            print(f"{self.name}: Error, age can't be negative")
+            print("Age update rejected")
+            if flag:
+                self._age = 10
+        else:
+            self._age = age
+            if not flag:
+                print(f"Age updated: {round(self._age)} days")
+
+    def get_height(self) -> float:
+        """returns protected value"""
+        return self._height
+
+    def get_age(self) -> int:
+        """returns protected value"""
+        return self._age
+
+    def show(self) -> str:
+        """formats data output-ready"""
+        return (f"{self.name}: "
+                f"{self._height:.1f}cm, "
+                f"{self._age} days old")
+
+
+class Flower(Plant):
+    """this is a class of a flower"""
+    def __init__(self, name: str, height: float, age: int, color: str):
+        super().__init__(name, height, age)
+        self.color = color
+    
+    def bloom():
+        print("Rose is blooming beautifully!")
+    
+    def show(self):
+        super().show()
+        print(f"Color: {self.color}")
+
+
+
+class Tree(Plant):
+    pass
+
+
+class Vegetable(Plant):
+    pass
+
+
+if __name__ == "__main__":
+    # executes as main program if not imported
+
+    print("=== Garden security system ===")
+    plant = Plant("Rose", 10, 10)
+    print(f"Plant created: {plant.show()}")
+    plant.set_age(30)
+    plant.set_age(-5)
+    print(f"Current state: {plant.show()}")
