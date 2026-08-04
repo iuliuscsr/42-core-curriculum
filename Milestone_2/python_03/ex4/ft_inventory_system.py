82cea67eccc7360e6a3817a7e58e4c6f709ca315
Julius Malsam <jmalsam@2-E-4.42heilbronn.de>
2026-08-04 10:36:23 +0200

    upload ex4

--- Milestone_2/python_03/ex4/ft_inventory_system.py
+++ Milestone_2/python_03/ex4/ft_inventory_system.py
@@ -2,8 +2,59 @@
 import sys
 
 
+def get_inventory() -> dict[str, int]:
+    """Parses input into an inventory dictionary."""
+
+    inventory: dict[str, int] = {}
+    for argc in sys.argv[1:]:
+        if ':' not in argc:
+            print(f"Error - invalid parameter '{argc}'")
+            continue
+        string: list[str] = argc.split(':', 1)
+        item = string[0].strip()
+        amount_str = string[1].strip()
+        if item in inventory:
+            print(f"Redundant item '{item}' - discarding")
+            continue
+        try:
+            amount = int(amount_str)
+            inventory[item] = amount
+        except ValueError as err:
+            print(f"Quantity error for 'key': {err}")
+    return inventory
+
+
+def display_inventory_information(inv: dict[str, int]) -> None:
+    """Displays multiple inventory stats."""
+
+    print(f"Got inventory: {inv}")
+    print(f"Item list: {list(inv.keys())}")
+    print(f"Total quantity of the {len(inv)} items: {sum(inv.values())}")
+
+    first_item, *rest = inv.keys()
+    name_max = first_item
+    name_min = first_item
+    tot_items = sum(inv.values())
+
+    for item in inv.keys():
+        percentage: float = round(inv[item] / tot_items * 100, 1)
+        print(f"Item {item} represents {percentage}%")
+        if inv[name_max] < inv[item]:
+            name_max = item
+        if inv[name_min] > inv[item]:
+            name_min = item
+
+    print(f"Item most abundant: {name_max} with quantity {inv[name_max]}")
+    print(f"Item least abundant: {name_min} with quantity {inv[name_min]}")
+    inv.update({"magic_item": 1})
+    print(f"Updated inventory: {inv}")
+
+
 def main() -> None:
-    pass
+    """Creates and displays inventory."""
+    print("=== Inventory System Analysis ===")
+    inventory = get_inventory()
+    display_inventory_information(inventory)
 
 
 if __name__ == "__main__":
